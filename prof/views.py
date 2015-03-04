6e14753841f0d87036110ffaff4a0bf965f364a6
thibaut <thibautlg@gmail.com>
2015-03-04 15:34:19 +0100

    Génératation des notes à télécharger en .csv corrigée.

--- prof/views.py
+++ prof/views.py
@@ -27,208 +27,20 @@ def is_prof(nom):
 
 
 def genererCSVnotes(qcm):
-	dossier=os.path.dirname(qcm.template.path)+"/"
+	dossier="media/ups/"+str(qcm.prof.id)+"/"+str(qcm.id)+"/"
 	codes=range(100000)
 	random.seed(float('0.'+str(qcm.id)))
 	random.shuffle(codes)
 	random.shuffle(codes)
 	random.shuffle(codes)
-	listecopies=qcm.copiecorrigee_set.all()
+	listecopies=[qcmpdf for qcmpdf in qcm.coreqcmpdf_set.all() if qcmpdf.reponses != ""]
 	fichierCSV=io.FileIO(dossier+"notes.csv",'w')
-	for copie in listecopies:
-		fichierCSV.write(str(codes[copie.numero])+","+copie.eleve.nom.encode('ascii','replace')+","+str(copie.note))
+	for qcmpdf in listecopies:
+		fichierCSV.write(str(codes[qcmpdf.numero])+","+qcmpdf.corecopie_set.all()[0].eleve.nom.encode('ascii','replace')+","+str(qcmpdf.getnote()))
 		fichierCSV.write("\n")
 	fichierCSV.close()
 	return str(dossier+"notes.csv")
 	
-
-
-def correction(cps):
-	
-	try:
-		dossier=os.path.dirname(cps.qcm.template.path)
-		os.mkdir(dossier+"/copies/"+str(cps.id))
-		sp.check_call(["convert", "-size", "1653x2338", cps.fichier.path, dossier+"/copies/"+str(cps.id)+"/copies.jpg"])
-
-		listepickle=[x for x in os.listdir(dossier+"/originaux") if x.endswith('.pickle')]
-		originaux=list()
-		for p in listepickle:
-			with open(dossier+"/originaux/"+p) as f:
-				originaux += pickle.load(f)
-
-		conf=qcmi.ConfigurationImport(dossier+"/")
-		conf.nmax=cps.qcm.nmax
-		listecopies=sorted([x for x in os.listdir(dossier+"/copies/"+str(cps.id)) if x.endswith('.jpg')], key=lambda r: int(''.join(x for x in r if x.isdigit())))
-		copies=list()
-		copies.append([qcmi.Eleve(dossier+"/copies/"+str(cps.id)+"/"+listecopies[0],conf)])
-		for i in range(len(listecopies)-1):	
-			cop=qcmi.Eleve(dossier+"/copies/"+str(cps.id)+"/"+listecopies[i+1],conf)
-		 ### les copies doivent être scannées dans l'ordre
-			if cop.code == copies[-1][0].code:
-				copies[-1].append(cop)
-			else:
-				copies.append([cop])
-
-		listeCodes=[o[0].code for o in originaux]
-		eleveinconnu,creation=Eleve.objects.get_or_create(nom="Élève non associé")
-		copiesasupprimer=list()
-		for copie in copies:
-
-			if not CopieCorrigee.objects.filter(numero=int(copie[0].code,2),qcm=cps.qcm):
-
-				cc=CopieCorrigee(copies=cps,numero=int(copie[0].code,2),eleve=eleveinconnu,qcm=cps.qcm)
-				cc.save()
-
-				try:
-					for i in range(len(copie)):
-						copie[i].compare(originaux[listeCodes.index(copie[i].code)][i])
-						ccjpg=CopieJPG(copiecorrigee=cc,fichier=copie[i].imgFichier)
-						ccjpg.save()
-
-				except Exception,er:
-					print('Erreur de correction: ',er)
-					cc.delete()
-					copiesasupprimer.append(copie)
-			else:
-				copiesasupprimer.append(copie)
-
-		for copie in copiesasupprimer:
-			copies.remove(copie)
-
-		listeRep=list()
-		for i in range(len(copies)):
-			listeRep.append([copies[i][0].code,"".join(copies[i][0].reponses)])
-			for j in range(len(copies[i])-1):
-				listeRep[-1][1]+="".join(copies[i][j+1].reponses)
-		fichierCSV=io.FileIO(dossier+"/copies/corr-"+str(cps.id)+".csv",'w')
-		for rep in listeRep:
-			fichierCSV.write(","+str(int(rep[0],2))+","+rep[1])
-			fichierCSV.write("\n")
-	       	fichierCSV.close()
-
-		reponsesElevesCSV=dossier+"/copies/corr-"+str(cps.id)+".csv"
-		CorrSortie=dossier+"/corrections.csv"
-	 #nomCSV=dossier+"/copies/notes-"+str(cps.id)+".csv"
-		corr=qcmc.correction(CorrSortie,reponsesElevesCSV)
-		corr.calculer()
-		for note in corr.note:
-			cc=CopieCorrigee.objects.get(numero=int(note[0]),copies=cps)
-			cc.note=note[1]
-			cc.save()
-
-		cps.corrigees=True
-		cps.save()
-	except Exception,er:
-		print("Erreur lors de la correction",er)
-		cps.corrigees=True
-		cps.save()
-
-
-def generateur(qcm):
-
-	nbexos=list()
-	listexosqcm=list()
-	for nb in sorted(NbExos.objects.filter(qcm=qcm), key=lambda r: int(r.id)):
-		nbexos.append(nb.nbexos)
-		listexosqcm.append(nb.exo.fichier.path)
-	config=ConfigurationMaker(steps=qcm.nbpdfs,numexam=qcm.id)
-	config.listeFichiers=listexosqcm
-	config.nbexos=nbexos
-	config.qcmid=qcm.id
-	config.nomQCM=qcm.nomTeX.encode('utf-8','ignore')
-	config.texteQCM=qcm.texteTeX.encode('utf-8','ignore')
-	qcm.nmax=int(np.trunc(np.log2(config.ntotal))+1)
-	qcm.save()
-	config.DossierSortie=os.path.dirname(qcm.template.path)+'/'
-	if 'originaux' not in os.listdir(config.DossierSortie):
-		os.mkdir(config.DossierSortie+'originaux')
-	if 'copies' not in os.listdir(config.DossierSortie):
-		os.mkdir(config.DossierSortie+'copies')
-
-	
-	#création d'une list() d'objets "liste d'exo" (i.e. un ficher.exos), une liste d'exo pour chaque fichier
-	exos=list()
-	for fichier in config.listeFichiers:
-		exos.append(listeExos(fichier,config))
-		
-		
-	#on créé notre classe CSV
-	sortieCSV=CSV(config.DossierSortie+config.CorrSortie,config.DossierSortie+config.QSortie)
-		
-	#on prépare la liste d'arguments pour le pdfunite de la sortie
-	pdfuniteArg=list()
-	pdfuniteArg.append('pdfunite')
-	paquet=1
-	
-	#on génère l'aléa qui code les n° des qcms
-	config.codes=range(100000)
-	random.seed(float('0.'+str(config.qcmid)))
-	random.shuffle(config.codes)
-	random.shuffle(config.codes)
-	random.shuffle(config.codes)
-	
-	listeqcmpdf=list()
-	
-	#on tire au hasard nexos[j] exos dans le fichier j, pour chaque fichier, et on répète pour avoir un total de ntotal codes tex
-	for i in range(config.ntotal):
-		k=0
-		#list() de la liste des exos choisis pour chaque version
-		listeExosSortie=list()
-		for j in config.nbexos:				#pour chaque fichier de liste d'exos, j est le nombre d'exos que l'on veut choisir à l'intérieur
-			if j==0:
-				j=exos[k].nbExos
-			r=random.sample(range(exos[k].nbExos),j)	#on en choisit donc j dans la k-ème colonne de exos (qui est dans le même ordre de la list() nexos)
-			listeExosSortie.append(r)						#on ajoute à notre liste() de versions ce random
-			sortieCSV.ajouterC(i,exos[k],r)					#on prévient le CSV
-			sortieCSV.ajouterQ(i,r)
-			k+=1											#on passe à la liste d'exos suivante
-		sortie=TeXify(config.DossierSortie+config.TeXTemplate,exos,listeExosSortie,str(bin(config.nfeuille+i)[2:]),config)		#on TeX tout ça
-	
-		#on écrit tout ça dans un .tex
-		fichierSortie=io.FileIO(config.DossierSortie+config.TeXSortie+str(i+config.nfeuille)+".tex",'w')
-		for ligne in range(len(sortie.TeX)):
-			fichierSortie.write(sortie.TeX[ligne])
-		fichierSortie.close()
-		#on le compile
-		sp.check_output(['pdflatex','-output-directory',config.DossierSortie,config.DossierSortie+config.TeXSortie+str(i+config.nfeuille)+'.tex'])
-      		#et on efface les fichiers inutiles
-		os.remove(config.DossierSortie+config.TeXSortie+str(i+config.nfeuille)+".aux")
-		os.remove(config.DossierSortie+config.TeXSortie+str(i+config.nfeuille)+".log")
-		#et on ajoute à pdfunite le ficher à concaténer ensuite
-		pdfuniteArg.append(config.DossierSortie+config.TeXSortie+str(i+config.nfeuille)+'.pdf')
-		#si on atteint une étape de concaténation
-		if i+1 in config.steps and i+1 != 1:
-			#on concatène les pdf
-			pdfuniteArg.append(config.DossierSortie+"originaux/"+config.TeXSortie+"-"+str(paquet)+'.pdf')
-			sp.call(pdfuniteArg)
-			fpdf=QcmPdf(qcm=qcm,fichier=config.DossierSortie+"originaux/"+config.TeXSortie+"-"+str(paquet)+'.pdf',nom=config.TeXSortie+"-"+str(paquet)+'.pdf')
-			fpdf.save()
-			listeqcmpdf.append(fpdf)
-			paquet+=1
-			pdfuniteArg=list()
-			pdfuniteArg.append('pdfunite')
-		elif i+1 in config.steps and i+1==1:
-			sh.copy(config.DossierSortie+config.TeXSortie+str(i+config.nfeuille)+'.pdf',config.DossierSortie+"originaux/"+config.TeXSortie+".pdf")
-			fpdf=QcmPdf(qcm=qcm,fichier=File(open(config.DossierSortie+"originaux/"+config.TeXSortie+".pdf", 'r')),nom=config.TeXSortie+".pdf")
-			fpdf.save()
-			listeqcmpdf.append(fpdf)
-			
-	#et on efface les pdf et tex de passage
-	for j in range(config.ntotal):
-		os.remove(config.DossierSortie+config.TeXSortie+str(j+config.nfeuille)+'.pdf')
-		os.remove(config.DossierSortie+config.TeXSortie+str(j+config.nfeuille)+'.tex')
-	
-	qcm.gen=1
-	qcm.save()
-	
-	#on sort le .csv des corrections
-	sortieCSV.imprimerC(config)
-	sortieCSV.imprimerQ(config.listeFichiers,config.nbexos,config)
-	
-	for qcmpdf in listeqcmpdf:
-		importoriginaux(qcmpdf)
-
-
 def telecharger(request,objet):
 	if isinstance(objet,models.FileField):
 		the_file = objet.path
@@ -246,7 +58,6 @@ def telecharger(request,objet):
 
 def svg(request,id_svg, prefix):
 	
-	print(id_svg,prefix)
 	if not request.user.is_active:
 		return redirect('django_cas.views.login')
 	nom = str(request.user.username)
@@ -284,7 +95,6 @@ def image(request,id_cc,page):
 	nom=str(request.user.username)
 	id_cc = int(id_cc)
 	page = int(page)
-	print(id_cc,page)
 	try:
 		el=Eleve.objects.get(nom=nom)
 		cc=CoreCopie.objects.get(id=id_cc)
@@ -440,7 +250,6 @@ def qcmaker(request):
 	formAjoutBanque.setListe(listebanques)
 
 	formDel=EffacerBanque(request.POST)
-	print(request.session['qcm'])	
 	if request.method == 'POST':  # S'il s'agit d'une requête POST
 	
 		#si nouveau QCM
@@ -517,7 +326,7 @@ def qcmaker(request):
 	try:
 		qcm=CoreQcm.objects.get(id=request.session['qcm'])
 	except Exception,er:
-		print('Erreur chargement du QCM'+str(request.session['qcm'])+': '+str(er))
+		print('Erreur chargement du QCM: '+str(er))
 		return redirect('prof.views.home')
 	if qcm.generation>0:
 		return redirect('prof.views.qcmanage')
